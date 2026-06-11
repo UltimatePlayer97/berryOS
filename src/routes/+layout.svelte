3078ef959e0a3f06632d6b3c8735395351206906
UltimatePlayer97 <gogole.help134@gmail.com>
2026-06-12 09:14:58 +1000

    feat: port webos to SvelteKit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
-	import './layout.css';
+  import "./layout.css";
+  import { base } from "$app/paths";
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
+
+<div
+  class="w-screen h-screen overflow-hidden bg-cover bg-center bg-no-repeat bg-fixed"
+  style="background-image: url('{base}/assets/background.jpg');"
+>
+  {@render children?.()}
+</div>
 
 <svelte:head></svelte:head>
 {@render children()}
