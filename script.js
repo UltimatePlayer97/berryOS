5cdfa2f15a66e61f3ae88545f8973e117ebdfdfc
UltimatePlayer97 <gogole.help134@gmail.com>
2026-06-11 19:52:28 +1000

    fix: properly follow the guide

--- script.js
+++ script.js
@@ -1,63 +1,58 @@
+// --- Time Display Logic ---
 let currentTime;
 let timeText = document.querySelector("#timeElement");
-timeText.innerHTML = currentTime;
 
 function updateTime() {
   currentTime = new Date().toLocaleString();
   timeText.innerHTML = currentTime;
 }
-
 setInterval(updateTime, 1000);
+updateTime();
 
-// https://www.w3schools.com/howto/howto_js_draggable.asp
-function dragElement(elmnt) {
-  const header = document.querySelector(".topbar");
-  const dragHandle = header || elmnt;
+function dragElement(element) {
+  var initialX = 0;
+  var initialY = 0;
+  var currentX = 0;
+  var currentY = 0;
 
-  let startX = 0;
-  let startY = 0;
-  let startLeft = 0;
-  let startTop = 0;
-
-  dragHandle.onmousedown = (e) => {
-    e.preventDefault();
-
-    startX = e.clientX;
-    startY = e.clientY;
-
-    const rect = elmnt.getBoundingClientRect();
-    startLeft = rect.left;
-    startTop = rect.top;
-
-    document.onmousemove = onDrag;
-    document.onmouseup = stopDrag;
-  };
-
-  function onDrag(e) {
-    e.preventDefault();
-
-    const dx = e.clientX - startX;
-    const dy = e.clientY - startY;
-
-    elmnt.style.left = startLeft + dx + "px";
-    elmnt.style.top = startTop + dy + "px";
+  if (document.getElementById(element.id + "header")) {
+    document.getElementById(element.id + "header").onmousedown = startDragging;
+  } else {
+    element.onmousedown = startDragging;
   }
 
-  function stopDrag() {
-    document.onmousemove = null;
+  function startDragging(e) {
+    e = e || window.event;
+    e.preventDefault();
+    initialX = e.clientX;
+    initialY = e.clientY;
+
+    document.onmouseup = stopDragging;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    currentX = initialX - e.clientX;
+    currentY = initialY - e.clientY;
+    initialX = e.clientX;
+    initialY = e.clientY;
+
+    element.style.top = element.offsetTop - currentY + "px";
+    element.style.left = element.offsetLeft - currentX + "px";
+  }
+
+  function stopDragging() {
     document.onmouseup = null;
+    document.onmousemove = null;
   }
 }
 
-dragElement(document.querySelector(".window"));
+dragElement(document.getElementById("welcome"));
 
-if (document.getElementById(elmnt.id + "header")) {
-  document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-} else {
-  elmnt.onmousedown = dragMouseDown;
-}
+var welcomeScreen = document.querySelector("#welcome");
 
-let welcomeScreen = document.querySelector("#welcome");
 function closeWindow(element) {
   element.style.display = "none";
 }
